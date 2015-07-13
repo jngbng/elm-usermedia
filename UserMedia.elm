4d51655f7b1d36d0a34a7a4b83f06ea150812dd5
giraj <jarl.flaten@epfl.ch>
2015-07-13 12:31:53 +0200

    add comments

--- UserMedia.elm
+++ UserMedia.elm
@@ -3,14 +3,27 @@ module UserMedia where
 import Native.UserMedia
 
 
-{- Actually Bool or MediaTrackConstraint allowing
-   for more specific requests/desires
+{- Sholuld actually be something like:
+
+        audio:Either Bool MediaTrackConstraint, video:Either Boold MediaTrackConstraint
+
+   (http://w3c.github.io/mediacapture-main/getusermedia.html#mediastreamconstraints)
 -}
 type alias MediaStreamConstraints
         = { audio:Bool, video:Bool }
 
+
+{- todo: implement
+   http://w3c.github.io/mediacapture-main/getusermedia.html#idl-def-MediaStream
+-}
 type alias MediaStream = {}
 
-getUserMedia : MediaStreamConstraints -> (MediaStream -> x) -> (e -> x)
+
+{- The return type of getUserMedia is Promise<MediaStream> which should be
+   translated into a corresponding Elm idiom.
+
+   Below, e : MediaStreamError, x and y are free variables.
+-}
+getUserMedia : MediaStreamConstraints -> (MediaStream -> x) -> (e -> y)
 getUserMedia =
     Native.UserMedia.getUserMedia
