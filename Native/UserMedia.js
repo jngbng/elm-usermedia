042e5412ed03040eece13ef28e9ec9a5323cdc52
giraj <jarl.flaten@epfl.ch>
2015-07-13 12:32:17 +0200

    add support for various browsers

--- Native/UserMedia.js
+++ Native/UserMedia.js
@@ -4,12 +4,25 @@ Elm.Native.UserMedia.make = function(elm) {
     elm.Native.UserMedia = elm.Native.UserMedia || {};
 
     if (elm.Native.UserMedia.values) return elm.Native.UserMedia.values;
+
+    navigator.mediaDevices = navigator.mediaDevices || ((navigator.mozGetUserMedia || navigator.webkitGetUserMedia) ? {
+        getUserMedia: function(c) {
+            return new Promise(function(y, n) {
+                (navigator.mozGetUserMedia ||
+                 navigator.webkitGetUserMedia).call(navigator, c, y, n);
+            });
+        }
+    } : null);
+
+    if (!navigator.mediaDevices) {
+        console.log("getUserMedia() not supported.");
+
+        // TODO: how should this be handled idiomatically in Elm?
+        return;
+    }
     
     var values = {};
-
-    values.getUserMedia = F3(function(constraints, successCallback, errorCallback) {
-        navigator.webkitGetUserMedia(constraints, successCallback, errorCallback);
-    });
+    values.getUserMedia = F3(navigator.mediaDevices.getUserMedia);
 
     return Elm.Native.UserMedia.values = values;
 }
