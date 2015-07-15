6b298f94fccd656433cb53507e23e33765d248da
giraj <giraj@users.noreply.github.com>
2015-07-15 09:26:35 +0200

    cleanup Example.elm

--- examples/Example.elm
+++ examples/Example.elm
@@ -1,9 +1,4 @@
 {- Sketch/idea of how one might bring getUserMedia into Elm
-
-   This doesn't work as I expect it to; it prompts the user for access to
-   the microphone (as desired), but it seems to me that the stream isn't
-   being forwarded to the userMediaStream mailbox (since the view isn't
-   updating)
 -} 
 
 import UserMedia exposing (MediaStream, requestUserMedia)
